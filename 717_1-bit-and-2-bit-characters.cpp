b0df10e63df7661be09e2019eadd593b5b03b6a4
Almer Bolatov <bolatov@users.noreply.github.com>
2017-11-08 10:43:01 +0600

    Leetcode P717
    717. 1-bit and 2-bit Characters

---
+++ 717_1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        for (int i = 0; i < bits.size();) {
+            if (i == bits.size() - 1) {
+                return !bits[i];
+            }
+            
+            if (bits[i] == 1) {
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        
+        return false;
+    }
+};
