c6589ec59f18a5477f76a8b49dea9a96e5d23452
Almer Bolatov <bolatov@users.noreply.github.com>
2018-01-25 12:14:25 +0600

    Leetcode P055
    55. Jump Game

---
+++ 055_jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int mx = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i <= mx) {
+                mx = max(mx, i + nums[i]);
+            }
+        }
+        return mx + 1 >= nums.size();
+    }
+};
