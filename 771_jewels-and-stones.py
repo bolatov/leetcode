e998e64e0cace030faf079182323a704f98af352
Almer Bolatov <almer.bolatov@gmail.com>
2018-02-28 11:23:22 +0600

    Leetcode P771

---
+++ 771_jewels-and-stones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return sum([(1 if ch in J else 0) for ch in S])
+        
