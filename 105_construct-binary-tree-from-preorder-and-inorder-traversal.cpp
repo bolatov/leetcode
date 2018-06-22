871b10c34ccf248418020c8dbbd3b9351673ac92
Almer Bolatov <bolatov@users.noreply.github.com>
2018-06-22 16:45:41 +0600

    LeetCode P105
    Add observation comments

--- 105_construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105_construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -7,6 +7,17 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// OBSERVATIONS:
+// 1. The very first element in preorder traversal is root element 
+//      followed by left sub-tree and right sub-tree.
+// 2. In inorder traversal array, the root element is in the middle,
+//      where elements to the left belong to the left sub-tree and
+//      elements to the right belong to the right sub-tree.
+// 
+//  e.g.
+//      PRE: [(1),2,4,5,3,6,7]
+//      INO: [4,2,5,(1),6,3,7]
 class Solution {
 public:
     int getPosition(vector<int>& vi, int k, int be, int en) {
