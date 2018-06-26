0f763ee2e261135e280f4bfb8583711aa2658c8c
Almer Bolatov <bolatov@users.noreply.github.com>
2018-06-26 19:16:28 +0600

    LeetCode P230
    Optimized solution

--- 230_kth-smallest-element-in-a-bst.cpp
+++ 230_kth-smallest-element-in-a-bst.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// O(n)
 class Solution {
 private:
     int cnt = 0;
@@ -22,5 +24,40 @@ public:
     int kthSmallest(TreeNode* root, int k) {
         inorder(root, k);
         return kthValue;
+    }
+};
+
+// O(log²n) using stack
+class Solution {
+public:
+    int getSize(TreeNode* root) {
+        if (!root) {return 0;}
+        stack<TreeNode*> st;
+        st.push(root);
+        int cnt = 0;
+        while (!st.empty()) {
+            auto top = st.top();
+            st.pop();
+            cnt++;
+            if (top->left) st.push(top->left);
+            if (top->right) st.push(top->right);
+        }
+        return cnt;
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> st;
+        st.push(root);
+        while (!st.empty()) {
+            auto top = st.top();
+            st.pop();
+            int size = getSize(top->left) + 1;
+            if (size == k) return top->val;
+            else if (size > k) st.push(top->left);
+            else {
+                k -= size;
+                st.push(top->right);
+            }
+        }
+        return -1;
     }
 };
