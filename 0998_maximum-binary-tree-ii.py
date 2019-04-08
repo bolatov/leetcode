f171e9f70ab333b9df5aeec61002570f82c5b771
Almer Bolatov <bolatov@users.noreply.github.com>
2019-04-08 17:02:16 +0200

    Create 0998_maximum-binary-tree-ii.py

---
+++ 0998_maximum-binary-tree-ii.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def insertIntoMaxTree(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        elif val > root.val:
+            node = TreeNode(val)
+            node.left = root
+            return node
+        else:
+            root.right = self.insertIntoMaxTree(root.right, val)
+            return root
+        
