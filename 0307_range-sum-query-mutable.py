9d9dfea6076047a115e57e1724bc3fc137cfd5f3
Almer Bolatov <bolatov@users.noreply.github.com>
2019-06-13 11:01:49 +0200

    Update 0307_range-sum-query-mutable.py

--- 0307_range-sum-query-mutable.py
+++ 0307_range-sum-query-mutable.py
@@ -1,3 +1,4 @@
+# Solution with the help of Segmentation Tree
 class NumArray:
 
     def __init__(self, nums: List[int]):
@@ -41,6 +42,50 @@ class NumArray:
                 return rsq(pos*2+2, mid+1, hi)
             return rsq(pos*2+1, lo, mid) + rsq(pos*2+2, mid+1, hi)
         return rsq(0, 0, self.n-1) if self.n > 0 else 0
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(i,val)
+# param_2 = obj.sumRange(i,j)
+
+
+# Much Faster solution with a Binary Indexed Tree a.k.a Fenwich Tree
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.n = len(nums)
+        self.nums = nums
+        self.tree = list(nums)
+        for i in range(1, self.n+1):
+            j = i + (i & -i)
+            if j <= self.n:
+                self.tree[j-1] += self.tree[i-1]
+        # print(f'Init\n\ttree={self.tree}\n')
+
+    def update(self, i: int, val: int) -> None:
+        k = i + 1
+        x = val - self.nums[i]
+        self.nums[i] = val
+        while k <= self.n:
+            self.tree[k-1] += x
+            k += k & -k
+        # print(f'Update\n\tnums={self.nums}\n\ttree={self.tree}\n')
+
+    def sumRange(self, i: int, j: int) -> int:
+        # print(f'Sum ({i},{j})')
+        def sumTill(k):
+            # print(f'\tsumTill({k})=', end='')
+            s = 0
+            while k >= 1:
+                s += self.tree[k-1]
+                k -= k & -k
+            # print(f'{s}')
+            return s
+        b = sumTill(j+1)
+        a = sumTill(i)
+        # print(f'\tsum({i},{j}) = {b-a}\n')
+        return b - a
 
 
 # Your NumArray object will be instantiated and called as such:
