744602c34f9be8c11743085dcc18f3a3addd4fe5
Almer Bolatov <bolatov@users.noreply.github.com>
2019-06-14 10:23:03 +0200

    Create 0636_exclusive-time-of-functions.py

---
+++ 0636_exclusive-time-of-functions.py
@@ -0,0 +1,20 @@
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        dct = collections.defaultdict(int)
+        stack = collections.deque([])
+        start_time = None
+        for log in logs:
+            tokens = log.split(':')
+            func_id, timestamp = int(tokens[0]), int(tokens[2])
+            if not stack:
+                stack.append((func_id, tokens[1], timestamp))
+            elif stack[-1][0] == func_id and stack[-1][1] != tokens[1]:
+                timestamp += 1
+                stack.pop()
+                dct[func_id] += timestamp - start_time
+            else:
+                prev_func_id = stack[-1][0]
+                dct[prev_func_id] += timestamp - start_time
+                stack.append((func_id, tokens[1], timestamp))
+            start_time = timestamp
+        return [dct[i] for i in range(n)]
