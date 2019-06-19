603693c25ea2d0ad305dce3bd627442dc5e6a064
Almer Bolatov <bolatov@users.noreply.github.com>
2019-06-19 15:10:49 +0200

    Update 0659_split-array-into-consecutive-subsequences.py

--- 0659_split-array-into-consecutive-subsequences.py
+++ 0659_split-array-into-consecutive-subsequences.py
@@ -1,27 +1,37 @@
 from heapq import heappush, heappop
 class Solution:
+    # Maintain a heap with each node being an end of the sequence.
+    # Each node in the heap holds a tuple with a tail element and count
+    # of elements in that sequence.
     def isPossible(self, nums: List[int]) -> bool:
         heap = []
         i = 0
         while i < len(nums):
             num = nums[i]
             if not heap:
+                # heap is empty, add the number and its count
                 heappush(heap, [num, 1])
                 i += 1
                 continue
             top, cnt = heap[0]
             if top+1 == num:
+                # current head can be augmented with a `num`
                 heappop(heap)
                 heappush(heap, [num, cnt+1])
                 i += 1
                 continue
             if top == num:
+                # current had cannot be augmented, start a new sequence
                 heappush(heap, [num, 1])
                 i += 1
                 continue
-            if cnt >= 3:
+            # current head (sequence) cannot be augmented anymore,
+            if cnt >= 3:  
+                # it has more than 3 elements in the sequence,
+                # remove it from the heap
                 heappop(heap)
                 continue
             else:
+                # it has less than 3 elements, return False
                 return False
         return not heap or all([cnt >= 3 for _, cnt in heap])
