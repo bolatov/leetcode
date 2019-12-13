9e76d8661fe4867904af903e2691cb977c6ca525
Almer Bolatov <bolatov@users.noreply.github.com>
2019-12-13 16:23:57 +0100

    Update 1239_maximum-length-of-a-concatenated-string-with-unique-characters.py

--- 1239_maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239_maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -33,3 +33,44 @@ class Solution:
             memo[key] = maxlen
         backtrack()
         return maxlen
+
+# a bit faster and cleaner solution
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        def get_mask(s):
+            mask = 0
+            for ch in s:
+                bit = ord(ch) - ord('a')
+                mask |= 1 << bit
+            return mask
+        masks = [get_mask(s) for s in arr if len(s) == len(set(s))]
+        memo = {}
+        def can_concat(word_mask, mask):
+            i = 0
+            while word_mask:
+                last_bit = word_mask & 1
+                if (last_bit << i) & mask:
+                    return False
+                i += 1
+                word_mask >>= 1
+            return True
+        def concat(word_mask, mask):
+            i = 0
+            while word_mask:
+                last_bit = word_mask & 1
+                word_mask >>= 1
+                mask |= last_bit << i
+                i += 1
+            return mask
+        def backtrack(idx, mask):
+            key = (idx, mask)
+            if key not in memo:
+                if idx >= len(masks):
+                    memo[key] = bin(mask).count('1')
+                else:
+                    curlen = 0
+                    if can_concat(masks[idx], mask):
+                        curlen = backtrack(idx+1, concat(masks[idx], mask))
+                    memo[key] = max(backtrack(idx+1, mask), curlen)
+            return memo[key]
+        return backtrack(0, 0)
